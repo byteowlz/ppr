97b3d9b6649846bc1cdd0c64eb91c9380367e1f7
Tommy Falkowski <tommy@byteowlz.com>
2025-08-16 20:37:47 +0200

    feat: add automated release pipeline

--- cmd/root.go
+++ cmd/root.go
@@ -7,6 +7,14 @@ import (
 	"github.com/spf13/cobra"
 )
 
+var (
+	versionInfo struct {
+		version string
+		commit  string
+		date    string
+	}
+)
+
 var rootCmd = &cobra.Command{
 	Use:   "ppr",
 	Short: "PPR - Programmable Palette Renderer",
@@ -15,7 +23,23 @@ It uses base16/base24 color schemes to generate beautiful wallpapers
 with customizable resolutions and automatic wallpaper setting.`,
 }
 
-func Execute() {
+var versionCmd = &cobra.Command{
+	Use:   "version",
+	Short: "Print version information",
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Printf("ppr version %s\n", versionInfo.version)
+		fmt.Printf("commit: %s\n", versionInfo.commit)
+		fmt.Printf("built: %s\n", versionInfo.date)
+	},
+}
+
+func Execute(version, commit, date string) {
+	versionInfo.version = version
+	versionInfo.commit = commit
+	versionInfo.date = date
+
+	rootCmd.Version = version
+
 	if err := rootCmd.Execute(); err != nil {
 		fmt.Println(err)
 		os.Exit(1)
@@ -32,4 +56,5 @@ func init() {
 	rootCmd.AddCommand(batchConvertCmd)
 	rootCmd.AddCommand(switchCurrentCmd)
 	rootCmd.AddCommand(cycleCmd)
+	rootCmd.AddCommand(versionCmd)
 }
--- main.go
+++ main.go
@@ -4,6 +4,12 @@ import (
 	"github.com/byteowlz/ppr/cmd"
 )
 
+var (
+	version = "dev"
+	commit  = "none"
+	date    = "unknown"
+)
+
 func main() {
-	cmd.Execute()
+	cmd.Execute(version, commit, date)
 }
